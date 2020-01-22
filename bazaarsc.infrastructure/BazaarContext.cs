9833f9aca5154f92c0c2ef167baa7da600bdb32b
alex <alex.km@live.com>
2020-01-22 18:28:23 +0400

    added database context and migrations

---
+++ bazaarsc.infrastructure/BazaarContext.cs
@@ -0,0 +1,32 @@
+﻿using bazaarcs.api.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace bazaarsc.infrastructure
+{
+    public class BazaarContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = LoadConnectionString();
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        private string LoadConnectionString()
+        {            
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false);
+            var configuration = builder.Build();
+            return configuration.GetConnectionString("BazaarDatabase");                        
+        }
+
+        public Listing Products { get; set; }
+        public Order Orders { get; set; }
+        public ActionLog ActionLogs { get; set; }
+        public ListingCategory ProductCategories { get; set; }
+        public Tag Tags { get; set; }
+    }
+}
--- bazaarsc.infrastructure/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace bazaarsc.infrastructure
-{
-    public class Class1
-    {
-    }
-}
